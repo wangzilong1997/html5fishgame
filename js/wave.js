5bb65cd2c7f092ae3300e010f0af1cc0b7bd13e7
wangzilong <1321787581@qq.com>
2019-08-03 16:23:45 +0800

    大鱼吃果实特效3

--- js/wave.js
+++ js/wave.js
@@ -12,27 +12,33 @@ waveObj.prototype.init = function() {
   }
 }
 waveObj.prototype.draw = function() {
+  ctx1.save()
+  ctx1.lineWidth = 2
+  ctx1.shadowBlur = 10
+  ctx1.shadowColor = "white"
   for(var i = 0;i < this.num; i++){
     if(this.alive[i]){
-        this.r[i] += deltaTime * 0.1
-        if(this.r[i] > 100){
+        this.r[i] += deltaTime * 0.03
+        if(this.r[i] > 50){
           this.alive[i] = false
+          break
         }
-        var alpha = 1 - this.r[i] / 100
+        var alpha = 1 - this.r[i] / 50
 
         ctx1.beginPath()
         ctx1.arc(this.x[i],this.y[i],this.r[i],0,Math.PI * 2)
         ctx1.closePath()
-        ctx1.strokeStyle = "rbga(255,255,255" + alpha + ")"
+        ctx1.strokeStyle = "rgba(255,255,255" + alpha + ")"
         ctx1.stroke()
     }
   }
+  ctx1.restore()
 }
 waveObj.prototype.born = function(x,y) {
   for(var i = 0;i < this.num; i++){
       if(!this.alive[i]){
         this.alive[i] = true
-        this.r[i] = 20
+        this.r[i] = 10
         this.x[i] = x
         this.y[i] = y
 
