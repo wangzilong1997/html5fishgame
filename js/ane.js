17abc24ad7df777ddad0d3157bf93c42e7bebd5c
wangzilong <1321787581@qq.com>
2019-08-03 19:31:40 +0800

    海葵摆动动画

--- js/ane.js
+++ js/ane.js
@@ -1,26 +1,38 @@
 var aneObj = function () {
-  this.x = []
-  this.len = []
+  //start point control point end point
+  this.rootx = []
+  this.headx = []
+  this.heady = []
+
+  this.amp = []
+  this.alpha = 0
 }
 
 aneObj.prototype.num = 50;
 aneObj.prototype.init = function() {
   for(var i = 0;i < this.num; i++){
-    this.x[i] = i * 16 + Math.random() * 20;
-    this.len[i] = 200 + Math.random() * 50;
+    this.rootx[i] = i * 16 + Math.random() * 20;
+    this.headx[i] = this.rootx[i]
+    this.heady[i] = canHeight - 250 + Math.random() * 50
+    this.amp[i] = Math.random() * 50 + 50
   }
-  console.log('a')
 }
 aneObj.prototype.draw = function() {
+
+  this.alpha += deltaTime * 0.0008
+  var l = Math.sin(this.alpha)
+
   ctx2.save();
   ctx2.globalAlpha = 0.6;
   ctx2.lineWidth = 20;
   ctx2.strokeStyle = "#3b154e";
   ctx2.lineCap = "round";
+
   for(var i = 0; i < this.num; i++){
+
     ctx2.beginPath();
-    ctx2.moveTo(this.x[i],canHeight);
-    ctx2.lineTo(this.x[i],canHeight - this.len[i]);
+    ctx2.moveTo(this.rootx[i],canHeight);
+    ctx2.quadraticCurveTo(this.rootx[i],canHeight - 100,this.headx[i] + l * this.amp[i],this.heady[i]);
     ctx2.stroke();
 
   }
